d745cef02b50d4f65deec4c7674ffc241e7d91ee
julesineza <j.ineza@alustudent.com>
2025-06-30 01:53:07 -0700

    file 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,5 @@
+#!usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range (1):
+            print("{} {} {}".format(matrix[i][j],matrix[i][j+1],matrix[i][j+2]))
