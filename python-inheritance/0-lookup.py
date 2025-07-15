fb255d54967844b5dcab444232c4c12b763a15b3
Jules <inezajul@gmail.com>
2025-07-15 14:39:55 +0200

    added the readme file and the first class

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """Function that return all the methods used in an object that has been passed as a parameter"""
+    return dir(obj)
+
+
---
+++ python-inheritance/README.md
@@ -0,0 +1,2 @@
+This repo covers part of oop consentating on inheritance in OOP which is how
+class are able to share attributes between the parent classes and child classes
