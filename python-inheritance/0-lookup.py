27941152035c3163b35015b9a797ca7934018988
Jules <inezajul@gmail.com>
2025-07-15 14:42:18 +0200

    added fixes

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Function that return all the methods used in an object that has been passed as a parameter"""
 def lookup(obj):
     """Function that return all the methods used in an object that has been passed as a parameter"""
     return dir(obj)
