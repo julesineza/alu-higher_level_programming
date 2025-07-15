3d6c6e3751edc914b63f78657e041cd5cf937157
Jules <inezajul@gmail.com>
2025-07-15 20:47:26 +0200

    added file 8 fix

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,21 +1,5 @@
 #!/usr/bin/python3
 
-"""Base Geometry class"""
-
-class BaseGeometry :
-    """area function returns an expection message"""
-    def area(self):
-        """"uses Expection to return the message """
-        raise Exception("area() is not implemented")
-    
-    def integer_validator(self, name, value):
-        """uses isinstance to check if value is an integer and also checks if value is less or equal to 0 and
-        raises errors"""
-        if not isinstance(value,int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0 :
-            raise ValueError(f"{name} must be greater than 0")
-        
 
 class Rectangle(BaseException):
     """Rectangle class inherits from BaseGeometry """
