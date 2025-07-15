55999ba3f096dc804f094306dff5e0aede96d418
Jules <inezajul@gmail.com>
2025-07-15 16:01:11 +0200

    added file 6

---
+++ python-inheritance/6-base_geometry.py.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Base Geometry class"""
+
+class BaseGeometry :
+    """area function returns an expection message"""
+    def area(self):
+        """"uses Expection to return the message """
+        return Exception("area() is not implemented")
+    
