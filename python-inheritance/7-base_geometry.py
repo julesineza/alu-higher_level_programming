5694d0e4c71e9b994d814e1a3683a71d6e4643fb
Jules <inezajul@gmail.com>
2025-07-15 19:56:50 +0200

    added file 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Base Geometry class"""
+
+class BaseGeometry :
+    """area function returns an expection message"""
+    def area(self):
+        """"uses Expection to return the message """
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """uses isinstance to check if value is an integer and also checks if value is less or equal to 0 and
+        raises errors"""
+        if not isinstance(value,int) :
+            raise TypeError("<name> must be an integer")
+        if value <= 0 :
+            raise ValueError("<name> must be greater than 0")
+        
+
