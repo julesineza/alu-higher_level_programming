c06a1d4ed98f7c61cd3cb00503d63001ddf0f535
Jules <inezajul@gmail.com>
2025-07-15 15:02:23 +0200

    fix

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,7 +2,7 @@
 """Defines the class MyList that extends list"""
 
 class MyList(list):
-
+    """MyList class inherites from list and has has print_sorted which returns a sorted list"""
 
     def print_sorted(self):
         """returns the list in ascending sorted order"""
