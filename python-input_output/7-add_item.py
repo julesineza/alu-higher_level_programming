3787911ef9335980f2430ed7d2ca433c64a6d293
Jules <inezajul@gmail.com>
2025-07-17 13:54:12 +0200

    fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-
 """
-a script that adds all arguments to a Python list, and then save them to a file
+Script that adds all arguments to a Python list, and then save them to a file.
 """
-import os 
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-if os.path.exists(filename):
+
+try:
     items = load_from_json_file(filename)
-else:
+except Exception:
     items = []
+
 items.extend(sys.argv[1:])
 save_to_json_file(items, filename)
