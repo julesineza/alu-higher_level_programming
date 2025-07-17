b2ebc9561a81464127f361f14edca1261c639a41
Jules <inezajul@gmail.com>
2025-07-17 13:21:44 +0200

    fix

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-import json
+
 """a function that returns the JSON representation of an object (string)"""
+
+import json
+
 def to_json_string(my_obj):
     """uses json.dumps to jsonify the my_obj parameter"""
     return json.dumps(my_obj)
