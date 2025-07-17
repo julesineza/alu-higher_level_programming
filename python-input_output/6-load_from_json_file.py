d45a5d12d3a5b8d4ff79b74fd2a1cc5b5840ce48
Jules <inezajul@gmail.com>
2025-07-17 13:35:07 +0200

    file 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+function that creates an Object from a “JSON file
+"""
+import json
+def oad_from_json_file(filename):
+    """
+    uses json.parse to convert data fromthe filename to an object
+    """
+
+    with open(filename,mode="r",encoding="utf-8") as myfile:
+        data=myfile.read()
+        json.parse(data)
