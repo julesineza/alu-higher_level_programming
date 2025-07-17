976997da4c2b3747c15fe6b97f41118a1bbcf881
Jules <inezajul@gmail.com>
2025-07-17 11:21:12 +0200

    started wotking on files

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""write file getts the filename and the text to be written in the file then writes to that files"""
+def write_file(filename="", text=""):
+    """we use with -> with open to write to a file and len() to get the length of the text parameter"""
+    with open("filename" , mode="w" , encoding="utf-8") as myfile:
+        myfile.write(text)
+        print(len(text))
+
+nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+print(nb_characters)        
---
+++ python-input_output/README.md
@@ -0,0 +1,1 @@
+In this repo i learnt how to write,append and read from a file using the utf-8 encoding
