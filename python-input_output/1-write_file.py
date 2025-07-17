b3ba95fb52ac112f9f9b56da9f404371fcc60c0a
Jules <inezajul@gmail.com>
2025-07-17 12:59:40 +0200

    fix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+
 """write file getts the filename and the text to be written in the file then writes to that files"""
+
 def write_file(filename="", text=""):
+
     """we use with -> with open to write to a file and len() to get the length of the text parameter"""
+
     with open(filename , mode="w" , encoding="utf-8") as myfile:
         myfile.write(text)
     return(len(text))
