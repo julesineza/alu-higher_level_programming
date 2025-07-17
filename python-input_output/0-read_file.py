4530f8be784f3ae5e1a6dc8615bc00e5347a355d
Jules <inezajul@gmail.com>
2025-07-17 13:14:27 +0200

    file 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyhton3
+
+"""a function that reads a text file (UTF8) and prints it to stdout"""
+
+def read_file(filename=""):
+    """use with and read mode to read the file that was given"""
+    with open(filename,encoding="utf-8") as myfile:
+        return myfile.read()
+
+
