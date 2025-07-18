6c012a3e77226ddd8245caf2b7fc6e2509185b58
Jules <inezajul@gmail.com>
2025-07-18 21:39:56 +0200

    added the whole sql _introduction directory

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- Write a script that deletes the database `hbtn_0c_0` in your MySQL server if the database doesn't exixt no error.
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- A script that lists the tables of a database in your MySQL server.
+SHOW TABLES;
