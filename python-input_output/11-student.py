6f07cfd404ce55137440c27accd94449021c8e5d
julesineza <j.ineza@alustudent.com>
2025-07-18 15:49:59 +0000

    added file 8-101

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with serialization methods.
+"""
+
+
+class Student:
+    """Defines a student by first_name, last_name, and age.
+    """
+    def __init__(self, first_name, last_name, age):
+        """Instantiate with first_name, last_name, and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dict representation of the Student.
+        If attrs is a list of strings, only attributes named in this list
+        will be returned.
+        """
+        if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
+            return {k: getattr(self, k)
+                    for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance."""
+        for key, value in json.items():
+            setattr(self, key, value)
