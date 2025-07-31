a641553cda799d17885c5f3c6a2f906808715a33
julesineza <j.ineza@alustudent.com>
2025-07-31 08:17:22 -0700

    addede the first file

---
+++ python-network_0/0-body_size.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Take the first argument as the URL
+url="$1"
+
+# Use curl to fetch the body (-s for silent, -o to throw away output, -w for size)
+curl -s -o /dev/null -w "%{size_download}\n" "$url"
