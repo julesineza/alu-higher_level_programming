b1544f936b958afa3e7ff1841102b09c571b53a2
Jules <inezajul@gmail.com>
2025-09-30 22:49:39 +0200

    fix

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,37 +1,8 @@
-#!/usr/bin/node
 
-class Rectangle {
-    constructor (w,h) {
-        this.width = w;
-        this.height = h;
-    }
+const Square5 = require('./5-square');
 
-    print() {
-        for (let i = 0; i < this.height; i++) {
-        let row = "";
-        for (let j = 0; j < this.width; j++) {
-            row += "X";
-        }
-        console.log(row);}
-    }
-
-    rotate () {
-        this.width=this.height
-        this.height=this.width
-    }
-
-    double () {
-        this.width = this.width *2
-        this.height = this.height * 2
-    }
-}
-
-class Square  extends Rectangle{
-    constructor (size) {
-        super(size,size)
-    }
-
-    charPrint(c) {
+class Square extends Square5 {
+  charPrint(c) {
     if (c === undefined) {
       c = 'X';
     }
@@ -41,6 +12,4 @@ class Square  extends Rectangle{
   }
 }
 
-
-
-module.exports =Square;
+module.exports = Square;
