245d7ba15f012637d8d422b0e105c8b6f2169f91
Jules <inezajul@gmail.com>
2025-09-30 23:17:06 +0200

    fix

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,14 +1,4 @@
 #!/usr/bin/node
-
-function nbOccurences (list, searchElement) {
-    let count = 0
-    for (let item of list) {
-        if (item === searchElement) {
-            count= count +1
-        }
-    }
-    return count
-}
-
-
-module.exports = nbOccurences;
+exports.nbOccurences = function (list, searchElement) {
+  return list.filter(e => e === searchElement).length;
+};
