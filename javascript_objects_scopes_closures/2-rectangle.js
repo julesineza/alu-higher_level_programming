078ce6328f9d5cfef33c3de5aaa5a68cd58647cc
Jules <inezajul@gmail.com>
2025-09-30 18:29:04 +0200

    file 2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,11 @@
 
 class Rectangle {
     constructor (w,h) {
-        this.width = w;
-        this.height = h;
+        if (!w <= 0 || !h <= 0){
+            this.width = w;
+            this.height = h;
+        }
     }
 }
+
+module.exports = Rectangle;
