ff3d6936d0e3fc6e587195d74ac79aebd7606619
Jules <inezajul@gmail.com>
2025-09-30 22:42:11 +0200

    file 6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -31,8 +31,16 @@ class Square  extends Rectangle{
         super(size,size)
     }
 
-    
+    charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
+
 
 
 module.exports =Square;
