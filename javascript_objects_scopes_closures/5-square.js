3c61145a1a408a184f9a7c9bd489014989384e2e
Jules <inezajul@gmail.com>
2025-09-30 22:31:02 +0200

    file 5

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor (w,h) {
+        this.width = w;
+        this.height = h;
+    }
+
+    print() {
+        for (let i = 0; i < this.height; i++) {
+        let row = "";
+        for (let j = 0; j < this.width; j++) {
+            row += "X";
+        }
+        console.log(row);}
+    }
+
+    rotate () {
+        this.width=this.height
+        this.height=this.width
+    }
+
+    double () {
+        this.width = this.width *2
+        this.height = this.height * 2
+    }
+}
+
+class Square  extends Rectangle{
+    constructor (size) {
+        super(size,size)
+    }
+}
+
+
+module.exports =Square;
