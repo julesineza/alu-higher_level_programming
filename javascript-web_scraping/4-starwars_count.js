c153f94c15f21de403db069bba40ac0b0f7ea186
Jules <inezajul@gmail.com>
2025-10-02 23:12:29 +0200

    fix

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-
 const request = require('request');
-
-const url = 'https://swapi-api.alx-tools.com/api/people/18';
+const url = process.argv[2];
 
 request(url, (error, response, body) => {
   if (error) {
@@ -12,7 +9,17 @@ request(url, (error, response, body) => {
     return;
   }
 
-  data = JSON.parse(body);
-  const movies = data.films.length;
-  console.log(movies);
+  // Parse JSON response
+  const data = JSON.parse(body);
+  const films = data.results;
+
+  // Count films where character ID 18 appears
+  let count = 0;
+  for (const film of films) {
+    if (film.characters.some(charUrl => charUrl.includes('/18/'))) {
+      count++;
+    }
+  }
+
+  console.log(count);
 });
