33a8a4ecdf3452238aafcdef329e7a05cfc59d7b
Jules <inezajul@gmail.com>
2025-10-02 22:07:55 +0200

    file 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,2 +1,15 @@
+#!/usr/bin/node
 
-args
+const fs = require("fs");
+const args = process.argv.slice(2)
+
+const filename= args[0]
+const text = args[1]
+
+fs.writeFile(filename,text, 'utf-8',(err) => {
+    if (err){
+        console.log(err)
+        return
+    }
+})
+
