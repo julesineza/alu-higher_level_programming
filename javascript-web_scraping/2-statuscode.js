65789843252233cb86ea73d493757809b686a915
Jules <inezajul@gmail.com>
2025-10-02 22:17:57 +0200

    file 2

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2)
+
+const request = require('request')
+const url = args[0]
+
+request(url,(error,response) => {
+    if (error){
+        console.log(error)
+        return
+    }
+    console.log(`code: ${response.statusCode}`)
+})
