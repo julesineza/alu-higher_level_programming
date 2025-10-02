9877ed5c13655079627032bd72e19eb0944111c7
Jules <inezajul@gmail.com>
2025-10-02 22:33:58 +0200

    file 3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node 
+
+const args = process.argv.slice(2)
+
+const request = require('request')
+const urlrequest = "https://swapi-api.alx-tools.com/api/films/" + args[0] + "/"
+
+request(urlrequest,(error,response,body) => {
+    if (error) {
+        console.log(error)
+        return
+    }
+    const data = JSON.parse(body)
+    console.log(data.title)
+})
